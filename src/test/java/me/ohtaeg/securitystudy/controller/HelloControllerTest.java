2f3d14befdc555441898de2e41bfd31f8fa84e7a
ohtaeg <otk1090@naver.com>
2021-04-28 00:22:38 +0900

    (test) : example code test 추가 - 현재 401 에러 발생

---
+++ src/test/java/me/ohtaeg/securitystudy/controller/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package me.ohtaeg.securitystudy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/apis/hello"))
+               .andExpect(status().isOk())
+               .andDo(print());
+    }
+}
